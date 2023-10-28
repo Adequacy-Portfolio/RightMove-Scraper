843b60b8faf2dbf3d10c69e97b815c28413d6d07
asterisk-a4v <asterisk@symplectic.link>
2023-10-28 11:06:58 +0100

    drna bzf swale77

---
+++ src/routes.ts
@@ -0,0 +1,82 @@
+import { Dataset, createPuppeteerRouter } from 'crawlee';
+import { selectors } from './selectors.js';
+
+export const router = createPuppeteerRouter();
+
+router.addDefaultHandler(async ({ log, page, enqueueLinks }) => {
+    log.info(`enqueueing new URLs`);
+    const title = await page.title()
+    log.info(title)
+
+/*     const names = await page.evaluate(()=>{
+        const list =[]
+        document.querySelectorAll('#l-searchResults > div > div > div > div > div.propertyCard-content > div.propertyCard-section > div.propertyCard-details > a > address').forEach(el => list.push(el.textContent))
+        return list 
+    }) */
+
+    // log.info(names.join(", "))
+    await enqueueLinks({
+        globs: ["https://www.rightmove.co.uk/properties/*([0-9])#/?channel*"],
+        label: "detail"
+    })
+
+
+    
+});
+
+
+router.addHandler('detail', async ({ request, page, log }) => {
+    const page_title = await page.title();
+    log.info(`${page_title}`, { url: request.loadedUrl });
+    const url = new URL(page.url())
+    const property_url = url.origin + url.pathname
+    
+    const title = await page.$eval(selectors.title, el => el.textContent)
+        .catch(() => null)
+    const price = await page.$eval(selectors.price, el => el.textContent)
+        .catch(() => null)
+    const date = await page.$eval(selectors.date, el => el.textContent)
+        .catch(() => null)
+    const type = await page.$eval(selectors.type, el => el.textContent)
+        .catch(() => null)
+    const bathroom = await page.$eval(selectors.bathroom, el => el.textContent)
+    .catch(() => null)
+    const bedroom = await page.$eval(selectors.bedroom, el => el.textContent)
+    .catch(() => null)
+    const tenure = await page.$eval(selectors.tenure ,el => el.textContent)
+    .catch(() => null)
+    const agent = await page.$eval(selectors.agent, el => el.textContent)
+    .catch(() => null)
+    const address = await page.$eval(selectors.address, el => el.textContent)
+    .catch(() => null)
+    const description = await page.$eval(selectors.description, el => el.textContent)
+    .catch(() => null)
+    const features = await page.$$eval(selectors.features, els => {
+        const list=[]
+        els.forEach((el)=>list.push(el.textContent))
+        return list })
+        .catch(() => null)
+    const floorplan = await page.$$eval(selectors.floorplan, els=> {        
+        const duck=[]
+        els.forEach((el)=>duck.push(el.getAttribute("href")))
+        return duck})
+        .then((results)=>{
+            return results.map((result)=>property_url+result)
+        })
+        .catch(() => null)
+    const pictures = await page.$$eval(selectors.pictures, els=> {        
+        const goose=[]
+        els.forEach((el)=>goose.push(el.getAttribute("href")))
+        return goose})
+        .then((results)=>{
+            return results.map((result)=>property_url+result)
+        })
+        .catch(() => null)
+    
+
+
+    await Dataset.pushData({
+        url: request.loadedUrl,
+        title,price,date,type,bathroom,tenure,agent,address,bedroom,description,features,floorplan,pictures
+    });
+});
---
+++ src/selectors.ts
@@ -0,0 +1,17 @@
+export const selectors = {
+    title: '#root > main > div > div.WJG_W7faYk84nW-6sCBVi > div > div.H2aPmrbOxrd-nTRANQzAY > div._1KCWj_-6e8-7_oJv_prX0H > div > h1',
+    price: '#root > main > div > div.WJG_W7faYk84nW-6sCBVi > div > article._2fFy6nQs_hX4a6WEDR-B-6 > div > div > div._1gfnqJ3Vtd1z40MlC0MzXu > span:nth-child(1)', 
+    date: '#root > main > div > div.WJG_W7faYk84nW-6sCBVi > div > article._2fFy6nQs_hX4a6WEDR-B-6 > div > div > div._1NmnYm1CWDZHxDfsCNf-WJ > div._1q3dx8PQU8WWiT7uw7J9Ck > div', 
+    type: '#root > main > div > div.WJG_W7faYk84nW-6sCBVi > div > article:nth-child(4) > div._4hBezflLdgDMdFtURKTWh > dl:nth-child(1) > div._3ZGPwl2N1mHAJH3cbltyWn > div._3OGW_s5TH6aUqi4uHum5Gy > dd', 
+    bathroom: '#root > main > div > div.WJG_W7faYk84nW-6sCBVi > div > article:nth-child(4) > div._4hBezflLdgDMdFtURKTWh > dl:nth-child(2) > div._3ZGPwl2N1mHAJH3cbltyWn > div._3OGW_s5TH6aUqi4uHum5Gy > dd',
+    bedroom: ' #root > main > div > div.WJG_W7faYk84nW-6sCBVi > div > article:nth-child(4) > div._4hBezflLdgDMdFtURKTWh > dl:nth-child(3) > div._3ZGPwl2N1mHAJH3cbltyWn > div._3OGW_s5TH6aUqi4uHum5Gy > dd',
+    tenure:'#root > main > div > div.WJG_W7faYk84nW-6sCBVi > div > article:nth-child(4) > div._4hBezflLdgDMdFtURKTWh > div > dl > div._3ZGPwl2N1mHAJH3cbltyWn > div > dd',
+    agent:'#root > main > div > div.WJG_W7faYk84nW-6sCBVi > aside > div > div > div._1HfIlGN38D_5t6P1dlQ4pW > div._2OyLeBVg-z92a2saGVUNnD > div > div.RPNfwwZBarvBLs58-mdN8 > a',
+    address:'#root > main > div > div.WJG_W7faYk84nW-6sCBVi > aside > div > div > div._1HfIlGN38D_5t6P1dlQ4pW > div._2OyLeBVg-z92a2saGVUNnD > div > div.OojFk4MTxFDKIfqreGNt0',
+    description:'#root > main > div > div.WJG_W7faYk84nW-6sCBVi > div > article:nth-child(5) > div.OD0O7FWw1TjbTD4sdRi1_ > div > div',
+    features:"#root > main > div > div.WJG_W7faYk84nW-6sCBVi > div > article:nth-child(5) > ul >li",
+    pictures:"#root > main > div > div.WJG_W7faYk84nW-6sCBVi > div > article:nth-child(5) > div.mtyLjuu2GD7KK4pvhCkS5 > div._3CFvsRHWaaSxrCfBezsRWO >a",
+    floorplan:'#root > main > div > div.WJG_W7faYk84nW-6sCBVi > div > article:nth-child(5) > div.mtyLjuu2GD7KK4pvhCkS5 > div.hU5084-VL7PQNtssjH2DK._234D-kZaWwhd0OYk2lgnix > div > a '
+
+
+}
